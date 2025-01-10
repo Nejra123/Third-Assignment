073996a577370a715dc868a777567da55791dedf
Nejra123 <nejra.zurapi@stu.ibu.edu.ba>
2025-01-10 16:39:42 +0100

    last update

--- js/theme.js
+++ js/theme.js
@@ -1,23 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const themeToggleButton = document.getElementById("lightmodebtn");
-    const body = document.body;
-  
+  const themeToggleButton = document.getElementById("lightmodebtn");
+  const body = document.body;
 
-    const savedTheme = localStorage.getItem("theme");
-  
+ 
+  const savedTheme = localStorage.getItem("theme");
 
-    if (savedTheme === "lightmode") {
+
+  if (savedTheme === "lightmode") {
+    body.classList.add("lightmode");
+    
+  } else {
+    body.classList.add("darkmode");  
+   
+  }
+
+ 
+  themeToggleButton.addEventListener("click", () => {
+    if (body.classList.contains("lightmode")) {
+   
+      body.classList.remove("lightmode");
+      body.classList.add("darkmode");
+     
+      localStorage.setItem("theme", "darkmode");
+    } else {
+
+      body.classList.remove("darkmode");
       body.classList.add("lightmode");
+     
+      localStorage.setItem("theme", "lightmode");
     }
-  
-    themeToggleButton.addEventListener("click", () => {
-      if (body.classList.contains("lightmode")) {
-        body.classList.remove("lightmode");
-        localStorage.setItem("theme", "darkmode");
-      } else {
-        body.classList.add("lightmode");
-        localStorage.setItem("theme", "lightmode");
-      }
-    });
   });
-  
+});
